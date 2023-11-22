3f0b5687b05d6576b92e4e56c112b41a4149a0f1
Christian Mahardhika <35090186+christianmahardhika@users.noreply.github.com>
2023-11-22 09:50:13 +0700

    add feature ticket update passenger

--- internal/data/dto_ticket/ticket.go
+++ internal/data/dto_ticket/ticket.go
@@ -1,13 +1,19 @@
 package dtoticket
 
 type RequestRedeemTicketToBPM struct {
-	CaseID     int64    `json:"case_id"`
-	IdNumbers  []string `json:"id_numbers"`
-	CodeTicket string   `json:"code_ticket"`
+	CaseID         int64               `json:"case_id"`
+	PassengersInfo []PassengerInfoData `json:"passengers_info"`
+	CodeTicket     string              `json:"code_ticket"`
+}
+
+type PassengerInfoData struct {
+	IDNumber        string `json:"id_number"`
+	VaccineStatus   string `json:"vaccine_status"`
+	BookingDetailID int64  `json:"booking_detail_id"`
 }
 
 type RequestUpdateTicket struct {
 	CodeTicket         string `json:"code_ticket"`
-	IsBoardingPass     bool   `json:"is_boarding_pass"`
+	BookingDetailID    int64  `json:"booking_detail_id"`
 	IsEligibleToFlight bool   `json:"is_eligible_to_flight"`
 }
